20845403f195a0157f5084a58be52a1f74777795
Stuart Corns <stuart.corns@clpt.co.uk>
2021-05-28 15:31:26 +0100

    New Look Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,43 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('css')
+<link href="{{ asset('css/admin.css') }}" rel="stylesheet" type="text/css" />
+@endsection
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <a href="/login/microsoft" class="btn">
-                <img style="height: 30px;" src="/images/svg/microsoft.svg" alt="">
-                Office 365
-            </a>
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+@section('content')
+    <div class="container">
+        <div class="flex direct-middle">
+            <div class="container-middle-1  container-margin-login flex-size margin-centre">
+                <div class="apollo">
+                    <img src="{{ asset('images/apollo-logo.svg')}}">
+                </div>
+                <div>
+                    <h3 class="padding-25-width text-center">Welcome to Apollo</h3>
+                </div>
+    
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+            <hr class="hr-break">
+            <div class="container-middle-2  container-margin-login flex-size">
+                <div class="margin-centre">
+                    <h3 class="centred">
+                        Sign In To Your Account
+                    </h3>
+                    <div class="centred button padding-25-height font20">
+                        <a href="#">Login</a>
+                    </div>
+                    <div>
+                        <div class="centred">
+                            <span></span>
+                            <h3 class="centred padding-25-height white">OR VIA</h3>
+                            <span></span>
+                        </div>
+                    </div>
+                    <a href="/login/microsoft" class="centred button padding-25-height font20">
+                            <img src="{{ asset('/images/svg/microsoft.svg')}}" alt="MS" height="30px" style="display:inline;">
+                            Office 365
                     </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-
-                <a href="/login/microsoft" class="inline-flex items-center px-4 py-2 bg-pink-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                    <img style="height: 30px;" src="{{asset('images/svg/microsoft.svg')}}" alt="">
-                    Office 365
-                </a>
+                </div>
             </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
-
-<div class="bg-danger p-4">
-    fgdfhdhfghfgh
-</div>
+        </div>
+    </div>
+@endsection
