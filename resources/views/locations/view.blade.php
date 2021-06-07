8fbccd770adea972168c7a454d12ef8630e4cb0a
Stuart Corns <stuart.corns@clpt.co.uk>
2021-06-07 11:02:00 +0100

    Test Push

--- resources/views/locations/view.blade.php
+++ resources/views/locations/view.blade.php
@@ -76,30 +76,7 @@
 
 @section('modals')
 
-<div class="modal fade" id="removeLocationModal" tabindex="-1" role="dialog"
-    aria-labelledby="removeLocationLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="removeLocationLabel">Are you sure you want to delete this Location?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Are you sure you would like to remove these <strong>Locations</strong> from the system?</p>
-                <small class="text-warning">**Warning this is permanent and the Assets assigned to this location will be set to Available.</small>
-            </div>
-            <div class="modal-footer">
-                <form  action="{{ route('location.destroy', ) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="dropdown-item" type="submit">Delete</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 @endsection
