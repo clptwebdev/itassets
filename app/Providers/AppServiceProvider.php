983a048ff1c8028facccc643310efa06d3cab03f
Stuart Corns <stuart.corns@heathpark.net>
2021-06-24 08:29:23 +0100

    App Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@ namespace App\Providers;
 
 use App\Models\Asset;
 use Illuminate\Support\ServiceProvider;
-use App\Models\Asset;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share("assetAmount" ,count(Asset::all()));
+        view()->share("assetAmount" ,count(\App\Models\Asset::all()));
     }
 }
