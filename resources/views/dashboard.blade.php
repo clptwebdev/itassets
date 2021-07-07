49c261013ba53ffb07c0f8442913b4b04d2d5a5f
Stuart Corns <stuart.corns@clpt.co.uk>
2021-07-07 09:34:44 +0100

    Test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@
                     <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                         aria-labelledby="dropdownMenuLink">
                         <div class="dropdown-header">Dropdown Header:</div>
-                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Action 1</a>
                         <a class="dropdown-item" href="#">Another action</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
