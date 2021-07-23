f31af620d4422f8929c42b63edb281b57dbc56bb
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2021-07-23 12:17:25 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share("assetAmount" ,count(\App\Models\Asset::all()));
+       
     }
 }
