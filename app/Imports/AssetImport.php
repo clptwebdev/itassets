305433d2cf4e6840409b07b59712fecc207deb69
Stuart Corns <stuart.corns@clpt.co.uk>
2021-09-23 14:16:39 +0100

    Assets Update

--- app/Imports/AssetImport.php
+++ app/Imports/AssetImport.php
@@ -186,7 +186,7 @@ class AssetImport implements ToModel, WithValidation, WithHeadingRow, WithBatchI
                 $fields = explode(';', $row['additional']);
                 foreach($fields as $field){
                     $field_value = explode(':', $field);
-                    if($found = Field::where(['name' => $field_value[0]])->get()){
+                    if($found = Field::where(['name' => $field_value[0]])->first()){
                         return dd($found);
                         $additional[$found->id] = ['value' => $field_value[1]];
                     }
