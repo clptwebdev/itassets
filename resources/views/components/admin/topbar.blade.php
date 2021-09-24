6fea61cc9195c047fd38175e1ccb633a1205155d
elliot.putt <elliot.putt@heathpark.net>
2021-09-24 12:47:12 +0100

    Neatening up users,Docs ank

--- resources/views/components/admin/topbar.blade.php
+++ resources/views/components/admin/topbar.blade.php
@@ -170,7 +170,7 @@
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Edit
                 </a>
-                <a class="dropdown-item" href="{{route("user.details")."/#Recent}}">
+                <a class="dropdown-item" href="{{route("user.details")."/#Recent"}}">
                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                     Activity Log
                 </a>
