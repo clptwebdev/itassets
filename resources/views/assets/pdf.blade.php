e67d5d68678afa9076832833c24cc646ad24ec02
Stuart Corns <stuart.corns@clpt.co.uk>
2021-09-24 13:40:52 +0100

    Assets PDF

--- resources/views/assets/pdf.blade.php
+++ resources/views/assets/pdf.blade.php
@@ -23,7 +23,13 @@
         <tr>
             <td>{{ $asset['name']}}<br>{{ $asset['model']}}</td>
             <td><span style="color: {{ $asset['icon']}}">{{$asset['location']}}</span></td>
-            <td align="center">#{{ $asset['asset_tag'] }}</td>
+            <td align="center">#{{ $asset['asset_tag'] ?? 'N/A'}}</td>
+            <td>{{ $asset['manufacturer']}}</td>
+            <td align="center">{{ $asset['purchased_date'] ?? 'N/A'}}</td>
+            <td align="center">{{ $asset['purchased_cost']}}</td>
+            <td>{{ $asset['supplier']}}</td>
+            <td align="center">{{ $asset['warranty']}}</td>
+            <td align="center">{{ $asset['audit'] ?? 'N/A'}}</td>
         </tr>
         @endforeach
     </table>
