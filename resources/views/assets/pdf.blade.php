4128c772c65bad8163b614f107c91d8974289d42
Stuart Corns <stuart.corns@clpt.co.uk>
2021-09-24 14:19:32 +0100

    Assets PDF

--- resources/views/assets/pdf.blade.php
+++ resources/views/assets/pdf.blade.php
@@ -21,6 +21,12 @@
         </tr>
         @foreach($assets as $id=>$asset)
         <tr>
+            <td>{{ $asset['name'] ?? 'N/A'}}<br>{{ $asset['model'] ?? 'N/A'}}</td>
+            <td><span style="color: {{ $asset['icon'] ?? '#666'}}">{{$asset['location'] ?? 'N/A'}}</span></td>
+            <td align="center">#{{ $asset['asset_tag'] ?? 'N/A'}}</td>
+            <td>{{ $asset['manufacturer'] ?? 'N/A'}}</td>
+            <td align="center">{{ $asset['purchased_date'] ?? 'N/A'}}</td>
+            <td align="center">{{ $asset['purchased_cost'] ?? 'N/A'}}</td>
             <td>{{ $asset['supplier'] ?? 'N/A'}}</td>
             <td align="center">{{ $asset['warranty'] ?? 'N/A'}}</td>
             <td align="center">{{ $asset['audit'] ?? 'N/A'}}</td>
