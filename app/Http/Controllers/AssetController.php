1e6e5f698753b6d360e15a5ccac1dafcdf2fc16d
Stuart Corns <stuart.corns@clpt.co.uk>
2021-11-01 15:57:15 +0000

    Asset Filter

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -685,7 +685,7 @@ class AssetController extends Controller {
         $assets ->join('locations', 'assets.location_id', '=', 'locations.id')
                 ->join('asset_models', 'assets.asset_model', '=', 'asset_models.id')
                 ->join('manufacturers', 'manufacturers.id', '=', 'asset_models.manufacturer_id')
-                ->orderBy(session('orderby') ?? 'purchased_date')->get();
+                ->orderBy(session('orderby') ?? 'purchased_date')->get(['assets.*', 'locations.name as location_name', 'manufacturers.name as manufactuer_name']);
         $limit = session('limit') ?? 25;
 
         return view('assets.view', [
