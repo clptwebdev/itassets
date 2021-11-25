31b75d3b29311934f241039670774e731e6e5b76
Stuart Corns <stuart.corns@heathpark.net>
2021-11-25 08:53:12 +0000

    Asset Filter

--- resources/views/components/filters/navigation.blade.php
+++ resources/views/components/filters/navigation.blade.php
@@ -1,4 +1,4 @@
-@props(['style','model'])
+@props(['style','model', 'filter'])
 <div class="d-flex justify-content-between flex-row-reverse mb-2 p-2">
     @php
         $route = strtolower($model);
