46b532c173b48e92ae123e3a3a4ca23e3e72b133
Stuart Corns <stuart.corns@heathpark.net>
2021-11-26 14:20:16 +0000

    Asset Bulk Options

--- resources/views/assets/view.blade.php
+++ resources/views/assets/view.blade.php
@@ -55,7 +55,17 @@
                 </form>
                 @endcan
             @endif
-            <button type="button" class="btn btn-sm btn-grey">Bulk Options</button>
+            <div class="dropdown show">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown link
+                </a>
+              
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
         </div>
     </div>
 
