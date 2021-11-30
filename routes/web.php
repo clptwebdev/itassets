80a44d1f0ff4f4f06e6540904d26372b319ee882
Stuart Corns <stuart.corns@heathpark.net>
2021-11-30 11:24:18 +0000

    Dashboard Speed Fix

--- routes/web.php
+++ routes/web.php
@@ -47,26 +47,21 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', function() {
         if(auth()->user()->role_id == 1)
         {
-            $locations = \App\Models\Location::with('asset', 'accessory', 'components', 'consumable', 'miscellanea', 'photo')->get();
+            //$locations = \App\Models\Location::with('asset', 'accessory', 'components', 'consumable', 'miscellanea', 'photo')->get();
             //Add ->with('depreciation_value');
-            $assets = \App\Models\Asset::with('location', 'supplier', 'model', 'fields', 'status', 'category', 'model')->get();
-            $assets->map(function($asset) {
-                $asset['deprecation_value'] = $asset->depreciation_value;
-
-                return $asset;
-            });
-            $transfers = \App\Models\Transfer::all();
-            $archived = \App\Models\Archive::all();
-            $statuses = \App\Models\Status::with('assets', 'accessory', 'components', 'consumable', 'miscellanea', 'accessories')->get();
+            $assets = \App\Models\Asset::with('location')->get();
+            $transfers = \App\Models\Transfer::count();
+            $archived = \App\Models\Archive::count();
+            $statuses = \App\Models\Status::all();
             $accessories = \App\Models\Accessory::all();
-            $requests = \App\Models\Requests::whereStatus(0)->get();
-            $components = App\Models\Component::with('supplier', 'location', 'status', 'manufacturer', 'category')->get();
+            $requests = \App\Models\Requests::whereStatus(0)->count();
+            $components = App\Models\Component::all();
             $consumables = App\Models\Consumable::all();
             $miscellaneous = App\Models\Miscellanea::all();
             $category = App\Models\Category::withCount('assets')->orderBy('id', 'asc')->with('assets', 'accessories', 'components', 'consumables', 'miscellanea')->take(6)->get();
         } else
         {
-            $locations = auth()->user()->locations;
+            //$locations = auth()->user()->locations;
             $assets = auth()->user()->location_assets;
             $transfers = \App\Models\Transfer::whereIn('location_from', $locations->pluck('id'))->orWhereIn('location_to', $locations->pluck('id'))->get();
             $archived = \App\Models\Archive::whereIn('location_id', $locations->pluck('id'))->get();
