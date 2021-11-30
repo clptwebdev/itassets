c73ca91ac5e228632cef0601eb69e36bddfabe2d
elliot.putt <elliot.putt@heathpark.net>
2021-11-30 13:30:49 +0000

    dashboard refactor

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -3,7 +3,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="d-flex flex-column align-items-center justify-content-center p-4" href="{{ route('dashboard') }}">
-        <div id="app-logo"><x-application-logo /></div>
+        <div id="app-logo">
+            <x-application-logo/>
+        </div>
     </a>
 
     <!-- Divider -->
@@ -18,69 +20,93 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
     <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('assets.index')}}" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-tablet-alt sidebar-icon" data-toggle="tooltip" data-placement="right" title="Components"></i>
+        <a class="nav-link collapsed" href="{{ route('assets.index')}}" data-toggle="collapse"
+           data-target="#collapseTwo" aria-expanded="true"
+           aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-tablet-alt sidebar-icon" data-toggle="tooltip" data-placement="right"
+               title="Components"></i>
             <span class="sidebar-title">Assets</span>
         </a>
         <div id="collapseTwo" class="collapse p-0" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <a class="sub-link collapse-item" href="{{ route('assets.index')}}"><i class="far fa-circle text-secondary"></i> All Assets (@if(auth()->user()->role_id == 1){{ \App\Models\Asset::all()->count()}}@else{{(auth()->user()->location_assets()->count()) ?? null}})@endif)</a>
-                @php
+            <a class="sub-link collapse-item" href="{{ route('assets.index')}}"><i
+                    class="far fa-circle text-secondary"></i> All Assets
+                (@if(auth()->user()->role_id == 1){{ \App\Models\Asset::all()->count()}}@else{{(auth()->user()->location_assets()->count()) ?? null}}
+                )@endif)</a>
+            @php
                 $statuses = App\Models\Status::all();
-                @endphp
-                <a href="#statusMenu" class="sub-link collapse-item collapsed d-none d-sm-block" data-toggle="collapse" data-parent="#statusMenu"><i class="fas fa-shield-alt fa-xs"></i> By Status</a>
-                <div class="collapse p-2" id="statusMenu">
-                    @foreach($statuses as $status)
-                    <a href="{{ route('assets.status', $status->id)}}" title="Add New Asset" class="collapse-item"><i class="{{$status->icon}} fa-xs" style="color: {{$status->colour}};"></i> {{ $status->name}}</a>
-                    @endforeach
-                </div>
-                <a href="#locationMenu" class="sub-link collapse-item collapsed d-none d-sm-block" data-toggle="collapse" data-parent="#locationMenu"><i class="fas fa-school fa-xs"></i> By Location</a>
-                <div class="collapse p-2" id="locationMenu">
-                    @php
-                        if(auth()->user()->role_id == 1){
-                            $locations = \App\Models\Location::all();
-                        }else {
-                            $locations = auth()->user()->locations;
-                        }
-                    @endphp
-                    @foreach($locations as $location)
-                    <a href="{{ route('assets.location', $location->id)}}" class="collapse-item" data-parent="#SubSubMenu1"><i class="far fa-circle" style="color:{{$location->icon}};"></i> {{ $location->name}}</a>
-                    @endforeach
-                </div>
-                <a href="{{ route('assets.bin')}}" title="Recycle Bin" class="collapse-item sub-link"><i class="fas fa-trash-alt fa-xs"></i> Recycle Bin</a>
-                <a href="{{ route('assets.create')}}" title="Add New Asset" class="collapse-item sub-link"><i class="fas fa-plus-circle fa-xs"></i> Add New Asset</a>
-                <a href="{{ route('transfers.assets')}}" title="Asset Transfers" class="sub-link collapse-item"><i class="fas fa-exchange-alt"></i> Transfers</a>
-                <a href="{{ route('archives.assets')}}" title="Asset Archives" class="sub-link collapse-item"><i class="fas fa-archive"></i> Disposed/Archived</a>
+            @endphp
+            <a href="#statusMenu" class="sub-link collapse-item collapsed d-none d-sm-block" data-toggle="collapse"
+               data-parent="#statusMenu"><i class="fas fa-shield-alt fa-xs"></i> By Status</a>
+            <div class="collapse p-2" id="statusMenu">
+                @foreach($statuses as $status)
+                    <a href="{{ route('assets.status', $status->id)}}" title="Add New Asset" class="collapse-item"><i
+                            class="{{$status->icon}} fa-xs" style="color: {{$status->colour}};"></i> {{ $status->name}}
+                    </a>
+                @endforeach
             </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('components.index')}}" data-toggle="collapse" data-target="#accessoryDD" aria-expanded="true"
-            aria-controls="accessoryDD">
-            <i class="fas fa-fw fa-keyboard sidebar-icon" data-toggle="tooltip" data-placement="right" title="Accessories"></i>
-            <span class="sidebar-title">Accessories</span>
-        </a>
-        <div id="accessoryDD" class="collapse p-0" aria-labelledby="accessoryTitle" data-parent="#accordionSidebar">
-                <a class="collapse-item sub-link" href="{{ route('accessories.index')}}"><i class="far fa-circle text-secondary"></i> View All</a>
-                <a class="collapse-item sub-link" href="{{ route('accessories.create')}}"><i class="fas fa-plus-circle fa-xs"></i> Add New Accessory</a>
-                <a href="{{ route('accessories.bin')}}" title="Recycle Bin" class="collapse-item sub-link"><i class="fas fa-trash-alt fa-xs"></i> Recycle Bin</a>
-                <a href="{{ route('transfers.accessories')}}" title="Accessory Transfers" class="collapse-item sub-link"><i class="fas fa-exchange-alt"></i> Transfers</a>
-                <a href="{{ route('archives.accessories')}}" title="Accessory Archives" class="collapse-item sub-link"><i class="fas fa-archive"></i> Disposed/Archived</a>
+            <a href="#locationMenu" class="sub-link collapse-item collapsed d-none d-sm-block" data-toggle="collapse"
+               data-parent="#locationMenu"><i class="fas fa-school fa-xs"></i> By Location</a>
+            <div class="collapse p-2" id="locationMenu">
+                @php
+                    if(auth()->user()->role_id == 1){
+                        $locations = \App\Models\Location::all();
+                    }else {
+                        $locations = auth()->user()->locations;
+                    }
+                @endphp
+                @foreach($locations as $location)
+                    <a href="{{ route('assets.location', $location->id)}}" class="collapse-item"
+                       data-parent="#SubSubMenu1"><i class="far fa-circle"
+                                                     style="color:{{$location->icon}};"></i> {{ $location->name}}</a>
+                @endforeach
+            </div>
+            <a href="{{ route('assets.bin')}}" title="Recycle Bin" class="collapse-item sub-link"><i
+                    class="fas fa-trash-alt fa-xs"></i> Recycle Bin</a>
+            <a href="{{ route('assets.create')}}" title="Add New Asset" class="collapse-item sub-link"><i
+                    class="fas fa-plus-circle fa-xs"></i> Add New Asset</a>
+            <a href="{{ route('transfers.assets')}}" title="Asset Transfers" class="sub-link collapse-item"><i
+                    class="fas fa-exchange-alt"></i> Transfers</a>
+            <a href="{{ route('archives.assets')}}" title="Asset Archives" class="sub-link collapse-item"><i
+                    class="fas fa-archive"></i> Disposed/Archived</a>
         </div>
     </li>
 
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('components.index')}}" data-toggle="collapse" data-target="#componentsDD" aria-expanded="true"
-            aria-controls="componentsDD">
-            <i class="far fa-fw fa-hdd sidebar-icon" data-toggle="tooltip" data-placement="right" title="Components"></i>
+        <a class="nav-link collapsed" href="{{ route('components.index')}}" data-toggle="collapse"
+           data-target="#accessoryDD" aria-expanded="true"
+           aria-controls="accessoryDD">
+            <i class="fas fa-fw fa-keyboard sidebar-icon" data-toggle="tooltip" data-placement="right"
+               title="Accessories"></i>
+            <span class="sidebar-title">Accessories</span>
+        </a>
+        <div id="accessoryDD" class="collapse p-0" aria-labelledby="accessoryTitle" data-parent="#accordionSidebar">
+            <a class="collapse-item sub-link" href="{{ route('accessories.index')}}"><i
+                    class="far fa-circle text-secondary"></i> View All</a>
+            <a class="collapse-item sub-link" href="{{ route('accessories.create')}}"><i
+                    class="fas fa-plus-circle fa-xs"></i> Add New Accessory</a>
+            <a href="{{ route('accessories.bin')}}" title="Recycle Bin" class="collapse-item sub-link"><i
+                    class="fas fa-trash-alt fa-xs"></i> Recycle Bin</a>
+            <a href="{{ route('transfers.accessories')}}" title="Accessory Transfers" class="collapse-item sub-link"><i
+                    class="fas fa-exchange-alt"></i> Transfers</a>
+            <a href="{{ route('archives.accessories')}}" title="Accessory Archives" class="collapse-item sub-link"><i
+                    class="fas fa-archive"></i> Disposed/Archived</a>
+        </div>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ route('components.index')}}" data-toggle="collapse"
+           data-target="#componentsDD" aria-expanded="true"
+           aria-controls="componentsDD">
+            <i class="far fa-fw fa-hdd sidebar-icon" data-toggle="tooltip" data-placement="right"
+               title="Components"></i>
             <span class="sidebar-title">Components</span>
         </a>
         <div id="componentsDD" class="collapse" aria-labelledby="componentsTitle" data-parent="#accordionSidebar">
-                <a class="collapse-item" href="{{ route('components.index')}}">View All</a>
-                <a class="collapse-item" href="{{route('components.create')}}"> Add New Component</a>
-                <a class="collapse-item" href="{{ route('components.index')}}"> Import Components</a>
+            <a class="collapse-item" href="{{ route('components.index')}}">View All</a>
+            <a class="collapse-item" href="{{route('components.create')}}"> Add New Component</a>
+            <a class="collapse-item" href="{{ route('components.index')}}"> Import Components</a>
         </div>
     </li>
 
@@ -97,9 +123,11 @@
         </div>
     </li> --}}
     <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('miscellaneous.index')}}" data-toggle="collapse" data-target="#miscellaneousDD" aria-expanded="true"
+        <a class="nav-link collapsed" href="{{ route('miscellaneous.index')}}" data-toggle="collapse"
+           data-target="#miscellaneousDD" aria-expanded="true"
            aria-controls="miscellaneousDD">
-            <i class="fas fa-fw fa-question sidebar-icon" data-toggle="tooltip" data-placement="right" title="Miscellaneous"></i>
+            <i class="fas fa-fw fa-question sidebar-icon" data-toggle="tooltip" data-placement="right"
+               title="Miscellaneous"></i>
             <span class="sidebar-title">Miscellaneous</span>
         </a>
         <div id="miscellaneousDD" class="collapse" aria-labelledby="consumableTitle" data-parent="#accordionSidebar">
@@ -125,7 +153,7 @@
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-            aria-controls="collapsePages">
+           aria-controls="collapsePages">
             <i class="fas fa-fw fa-users sidebar-icon"></i>
             <span class="sidebar-title">Users</span>
         </a>
@@ -166,20 +194,23 @@
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#settingPages" aria-expanded="true"
-            aria-controls="settingPages">
+           aria-controls="settingPages">
             <i class="fas fa-fw fa-cogs sidebar-icon"></i>
             <span class="sidebar-title">Settings</span>
         </a>
         <div id="settingPages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <a class="collapse-item" href="{{ route('asset-models.index')}}">Asset Models</a>
-                <a class="collapse-item" href="{{ route('depreciation.index')}}">Depreciation</a>
-                <a class="collapse-item" href="{{ route('category.index')}}">Categories</a>
-                <a class="collapse-item" href="{{ route('fieldsets.index')}}">Fieldsets</a>
-                <a class="collapse-item" href="{{ route('fields.index')}}">Custom Fields</a>
-                <a class="collapse-item" href="{{ route('status.index')}}">Status Fields</a>
-                <a class="collapse-item" href="/databasebackups">Database Backups</a>
-                <a href="{{ route('reports.index')}}" class="collapse-item">Generated Reports</a>
-                <a class="collapse-item" href="{{ route("documentation.index") }}">Documentation</a>
+            <a class="collapse-item" href="{{ route('asset-models.index')}}">Asset Models</a>
+            <a class="collapse-item" href="{{ route('depreciation.index')}}">Depreciation</a>
+            <a class="collapse-item" href="{{ route('category.index')}}">Categories</a>
+            <a class="collapse-item" href="{{ route('fieldsets.index')}}">Fieldsets</a>
+            <a class="collapse-item" href="{{ route('fields.index')}}">Custom Fields</a>
+            <a class="collapse-item" href="{{ route('status.index')}}">Status Fields</a>
+
+            @if(auth()->user()->role_id == 1)    <a class="collapse-item" href="/databasebackups">Database
+                Backups</a>@endif
+
+            <a href="{{ route('reports.index')}}" class="collapse-item">Generated Reports</a>
+            <a class="collapse-item" href="{{ route("documentation.index") }}">Documentation</a>
         </div>
     </li>
 
