63e07b4546f2967141c5c7b1d499a56c3d2cc9f0
Stuart Corns <stuart.corns@heathpark.net>
2021-12-02 14:11:33 +0000

    Asset Filter

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -749,9 +749,8 @@ class AssetController extends Controller {
                 ->orderBy(session('orderby') ?? 'purchased_date' , session('direction') ?? 'asc')
                 ->paginate(intval(session('limit')) ?? 25, ['assets.*', 'asset_models.name as asset_model_name', 'locations.name as location_name', 'manufacturers.name as manufacturer_name', 'suppliers.name as supplier_name'])
                 ->fragment('table');
-        return dd($assets->get());
         return view('assets.view', [
-            "assets" => $assets,
+            "assets" => $assets->get(),
             'suppliers' => Supplier::all(),
             'statuses' => Status::all(),
             'categories' => Category::all(),
