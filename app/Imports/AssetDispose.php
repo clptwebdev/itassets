6f2c5805782cfa50860797093f810fdf741201f0
Stuart Corns <stuart.corns@heathpark.net>
2021-12-03 12:31:39 +0000

    Asset Bulk Dispose

--- app/Imports/AssetDispose.php
+++ app/Imports/AssetDispose.php
@@ -63,11 +63,11 @@ class AssetDispose implements ToModel, WithValidation, WithHeadingRow, WithBatch
 
     public function rules(): array
     {
-        //Asset Tag create rule to check to see if it exists in the same location
+        //Asset Tag create rule to check to see if it exists in the same location [SC]
         return [
             'date' => [
-                'sometimes',
-                'nullable',
+                'date_format:"d/m/Y"',
+                'required',
             ], 
             'reason' => [
                 'required',
@@ -82,7 +82,65 @@ class AssetDispose implements ToModel, WithValidation, WithHeadingRow, WithBatch
 
     public function model(array $row)
     {
-       
+        $asset = $this->asset;
+        /* If the Validation has been passed successfully */
+        /* Create a new request to dispose this Asset [SC]*/
+        $requests = Requests::create([
+            'type'=>'disposal', 
+            'model_type'=> 'asset', 
+            'model_id'=>$asset->id,
+            'notes' => $row['reason'],
+            'date' => $row['date'],
+            'user_id' => auth()->user()->id, 
+            'status' => 0,
+        ]);
+
+        /* If the User is a Super Admin, we can skip the approval */
+        /* Moves the Asset to the Archive [SC] */
+        
+        if(auth()->user()->role_id == 1){
+            /* Get the Depreciaiton Years from the Asset Model or 0 */
+            /* This is to calculate the Value of the Asset at the time of Disposal [SC] */
+            $years = $asset->model->depreciation->years ?? 0
+            $eol = \Carbon\Carbon::parse($asset->purchased_date)->addYears($years);
+            if($eol->isPast()){
+                $dep = 0;
+            }else{
+                $age = \Carbon\Carbon::now()->floatDiffInYears($asset->purchased_date);
+                $percent = 100 / $years;
+                $percentage = floor($age)*$percent;
+                $dep = $asset->purchased_cost * ((100 - $percentage) / 100);
+            }
+
+            $archive = Archive::create([
+                'model_type' => 'asset',
+                'name' => $asset->name ?? 'No Name',
+                'asset_tag' => $asset->asset_tag ?? 'No Asset Tag',
+                'serial_no' => $asset->serial_no ?? 'N/A',
+                'asset_model' => $asset->model->name ?? 'No Model',
+                'order_no' => $asset->order_no ?? 'N/A',
+                'supplier_id' => $asset->supplier_id ?? 0,
+                'purchased_date' => $asset->purchased_date,
+                'purchased_cost' => $asset->purchased_cost,
+                'archived_cost' => number_format($dep, 2),
+                'warranty' => $asset->warranty,
+                'location_id' => $asset->location_id ?? 0,
+                'room' => $asset->room ?? 'N/A',
+                'logs' => 'N/A',
+                'comments' => 'N/A',
+                'created_user' => $asset->user_id ?? 0,
+                'created_on' => $asset->created_at,
+                'user_id' => auth()->user()->id,
+                'super_id' => auth()->user()->id,
+                'date' => \Carbon\Carbon::now()->format('Y-m-d'),
+                'notes' => $row['reason'],
+            ]);
+            $asset->forceDelete();
+            $requests->update(['status' => 1, 'super_id'  => auth()->user()->id, 'updated_at' => \Carbon\Carbon::now()->format('Y-m-d')]);
+        }else{
+            /* Send an Email to Super Admin requesting the Disposal */
+        }
+
     }
 
     public function batchSize(): int
@@ -90,8 +148,4 @@ class AssetDispose implements ToModel, WithValidation, WithHeadingRow, WithBatch
         return 1000;
     }
 
-    public function uniqueBy()
-    {
-        return 'asset_tag';
-    }
 }
