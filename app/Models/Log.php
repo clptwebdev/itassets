877fdb020fb0978528cb843ccf99d7da4227e565
elliot.putt <elliot.putt@heathpark.net>
2021-12-07 12:01:14 +0000

    export css

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -9,32 +9,39 @@ use \App\Models\User;
 use \App\Models\Asset;
 use \App\Models\Component;
 
-class Log extends Model
-{
+class Log extends Model {
+
     use HasFactory;
 
     protected $fillable = ['user_id', 'loggable_type', 'loggable_id', 'data', 'created_at'];
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function asset(){
+    public function asset()
+    {
         return $this->belongsTo(Asset::class);
     }
 
-    public function component(){
+    public function component()
+    {
         return $this->belongsTo(Component::class);
     }
+
     public function miscellanea()
     {
         return $this->belongsTo(Miscellanea::class);
     }
-    public function scopeLogFilter($query , array $filters){
-        $query->when($filters['search'] ?? false , fn($query ,$search) =>
-        $query->where('data','like','%' . $search. '%')
-        ->orWhere('loggable_type','like','%' . $search. '%')
-        );
+
+    public function scopeLogFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) => $query->where('data', 'like', '%' . $search . '%')
+            ->orWhere('loggable_type', 'like', '%' . $search . '%')
+            ->whereIn('user-id', $search)
+    );
 
     }
+
 }
