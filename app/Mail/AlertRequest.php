0fcc9db7b54f0d119e5888d74901be42c4c7c65d
Stuart Corns <stuart.corns@heathpark.net>
2021-12-08 10:20:47 +0000

    Request Notification

--- app/Mail/AlertRequest.php
+++ app/Mail/AlertRequest.php
@@ -29,7 +29,7 @@ class AlertRequest extends Mailable
     public function __construct(User $user, $requests_type, $requests_id, $requests_from, $requests_to, $requests_date, $requests_comment)
     {
         $this->user = $user;
-        $this->requests_type = $comment;
+        $this->requests_type = $requests_type;
         $m = "\\App\\Models\\".ucfirst($type);
         $this->requests_model = $m::find($id);
         $this->requests_from = Location::find($from);
