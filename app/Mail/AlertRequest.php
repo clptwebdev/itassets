b12d5437569aaf8cf8c561247f150d93b6e5b8f9
Stuart Corns <stuart.corns@heathpark.net>
2021-12-08 10:20:09 +0000

    Request Notification

--- app/Mail/AlertRequest.php
+++ app/Mail/AlertRequest.php
@@ -14,20 +14,28 @@ class AlertRequest extends Mailable
     use Queueable, SerializesModels;
 
     public $user;
-    public $model;
-    public $from;
-    public $to;
-    public $date;
-    public $type;
-    public $comment;
+    public $requests_model;
+    public $requests_from;
+    public $requests_to;
+    public $requests_date;
+    public $requests_type;
+    public $requests_comment;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user, $requests_type, $requests_id, $requests_from, $requests_to, $requests_date, $requests_comment)
     {
+        $this->user = $user;
+        $this->requests_type = $comment;
+        $m = "\\App\\Models\\".ucfirst($type);
+        $this->requests_model = $m::find($id);
+        $this->requests_from = Location::find($from);
+        $this->requests_to = Location::find($to);
+        $this->requests_date = $date;
+        $this->requests_comment = $comment;
     }
 
     /**
