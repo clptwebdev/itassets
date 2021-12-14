454a67d74c9361c3c28534d6f86c9673806b4f70
Stuart Corns <stuart.corns@heathpark.net>
2021-12-14 10:29:56 +0000

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,5 +7,21 @@
 @endsection
 
 @section('content')
+    <!-- session messages -->
+    <x-handlers.alerts/>
 
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    </div>
+
+    @if($assets->count() != 0)
+        <!-- Asset stats -->
+        <x-admin.asset-info :transfers=$transfers :archived=$archived :assets=$assets :accessories=$accessories
+                            :components=$components :consumables=$consumables :miscellaneous=$miscellaneous
+                            :requests=$requests/>
+        <x-categories_status_info :statuses="$statuses" :category="$category"/>
+    @elseif(auth()->user()->role_id == 0)
+                <x-admin.request-access/>
+    @endif
 @endsection
