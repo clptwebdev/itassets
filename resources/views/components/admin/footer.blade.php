8b277051aa605237b89a5c96153c3c4513d7fb58
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2022-01-04 09:38:29 +0000

    Update footer.blade.php

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
             <span id="copyright">A Central Learning Partnership Trust System | Copyright &copy; Apollo Asset Management
-                2021</span>
+                2022</span>
         </div>
     </div>
 </footer>
