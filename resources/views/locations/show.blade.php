626334d010c03d8395e868d63e748cf4519c3c06
Stuart Corns <stuart.corns@tandonta.com>
2022-01-05 11:25:58 +0000

    Location View

--- resources/views/locations/show.blade.php
+++ resources/views/locations/show.blade.php
@@ -63,6 +63,7 @@
                                     {{ $location->postcode }}</p>
                                 <p>Tel: {{ $location->telephone }}</p>
                                 <p>Email: {{ $location->email }}</p>
+                                <p>* Spent £{{number_format($location->expenditure('2021'), 11, 2)}} in the Year 2021</p>
                             </div>
                         </div>
                     </div>
