29c62750f82221a220e6a571df7e84d9a224679a
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2022-01-10 12:20:46 +0000

    asset create input switch workaround

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -9,22 +9,11 @@
         @if(str_contains($formAttributes,'required' ))<span class="text-danger">*</span>@endif
     @endif
 @endif
-<input type="{{$type}}" class="form-control
-       {{-- for assets switch case feedback--}}
-@if($switch == false)
-@if($errors->has('{!! $name !!}'))  {!! 'border-danger' !!}@endif
-@elseif($switch == true)
-@if ($errors->has(str_replace(' ', '_', strtolower($name))))  {!! 'border-danger' !!} @endif
-@endif
-    "
-       @if($switch == false)
-       name="{{$name}}"
-       value="{{old("{!! $name !!}") ?? $value}}"
-       @elseif($switch == true)
-       name="{{str_replace(' ', '_', strtolower($field->name))}}"
-       value="{{ old(str_replace(' ', '_', strtolower($field->name)))}}"
-       @endif
-       {{-- END--}}
-       id="{{$name}}" placeholder="{{str_replace(array('_','id'), ' ',ucfirst($title ?? $name))}}"
+<input type="{{$type}}"
+       class="form-control@if ($errors->has(str_replace(' ', '_', strtolower($name))))  {!! 'border-danger' !!} @endif"
+       name="{{str_replace(' ', '_', strtolower($name))}}"
+       value="{{ old(str_replace(' ', '_', strtolower($name)))}}"
+       id="{{ old(str_replace(' ', '_', strtolower($name)))}}"
+       placeholder="{{str_replace(array('_','id'), ' ',ucfirst($title ?? $name))}}"
     {!!$formAttributes ?? null!!}
 >  {{--  pass attribues seperated with spaces  --}}
