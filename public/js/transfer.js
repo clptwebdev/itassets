0eaae84211ef5759440a805e39e19365121c4ded
Stuart Corns <stuart.corns@heathpark.net>
2022-01-11 12:19:40 +0000

    Asset Transfer

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -1,7 +1,8 @@
 $('.transferBtn').click(function () {
-    const modelID = document.querySelector('#model_id').value = this.dataset.model - id;
-    const modelTAG = document.querySelector('#asset_tag_transfer').value = this.dataset.model - tag;
-    const locationID = document.querySelector('#location_id').value = this.dataset.location - id;
-    const locationFROM = document.querySelector('#location_from').value = this.dataset.location - from;
+    const modelID = document.querySelector('#model_id').value = this.getAttribute('data-model-id');
+    const modelTAG = document.querySelector('#asset_tag_transfer').value = this.getAttribute('data-model-tag');
+    const locationID = document.querySelector('#location_id').value = this.getAttribute('data-location-id');
+    const locationFROM = document.querySelector('#location_from').value = this.getAttribute('data-location-from');
     $('#requestTransfer').modal('show');
+    //const requestModal = document.querySelector('#requestTransfer').style.display = 'block';
 });
