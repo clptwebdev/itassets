b1ebc4225992e10c0db8135aecd9c6a18b9f55a4
Stuart Corns <stuart.corns@tandonta.com>
2022-01-19 22:28:09 +0000

    Image Upload

--- public/js/photo.js
+++ public/js/photo.js
@@ -1,35 +1,38 @@
-const profileImage = document.querySelector("profileImage");
-const photoId = document.querySelector("photoId");
 
-function selectPhoto(id, src) {
-    profileImage.src = src;
-    photoId.value = id;
-    $("#imgModal").modal("hide");
-}
+(() => {
+    const profileImage = document.querySelector("profileImage");
+    const photoId = document.querySelector("photoId");
 
-$(document).ready(function () {
-    $("form#imageUpload").submit(function (e) {
-        e.preventDefault();
-        var formData = new FormData(this);
-        var urlto = "/photo/upload";
-        var route = '{{asset("/")}}';
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-        });
-        // AJAX request
-        $.ajax({
-            url: urlto,
-            method: "POST",
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function (data) {
-                $("#uploadModal").modal("hide");
-                profileImage.src = route + data.path;
-                photoId.value = data.id;
-            },
+    function selectPhoto(id, src) {
+        profileImage.src = src;
+        photoId.value = id;
+        $("#imgModal").modal("hide");
+    }
+
+    $(document).ready(function () {
+        $("form#imageUpload").submit(function (e) {
+            e.preventDefault();
+            var formData = new FormData(this);
+            var urlto = "/photo/upload";
+            var route = '{{asset("/")}}';
+            $.ajaxSetup({
+                headers: {
+                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+                },
+            });
+            // AJAX request
+            $.ajax({
+                url: urlto,
+                method: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (data) {
+                    $("#uploadModal").modal("hide");
+                    profileImage.src = route + data.path;
+                    photoId.value = data.id;
+                },
+            });
         });
     });
-});
+}()
