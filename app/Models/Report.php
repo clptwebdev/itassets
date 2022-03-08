a40ef0793523bf8f0065e714cc8783fac1d6f44f
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2022-03-08 08:16:13 +0000

    schedular updated

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -4,15 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
-class Report extends Model
-{
+class Report extends Model {
+
     use HasFactory;
+
     protected $fillable = ['id', 'report', 'user_id', 'created_at'];
 
-
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    public static function clean()
+    {
+        $table = \DB::table('reports')->get();
+        foreach($table as $entry)
+        {
+            $entry->delete();
+        }
+        $files = Storage::files('/public/reports');
+        Storage::delete($files);
+
+        return true;
     }
 
 }
