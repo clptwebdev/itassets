441b2b247278cd7b97a797b8edfc30bfaba38b9e
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2022-03-15 08:32:53 +0000

    backups test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,43 +32,43 @@ class Kernel extends ConsoleKernel {
     {
         $schedule->call('\App\Http\Controllers\BackupController@createDb')->everyMinute();
         //cleans all backups Monthly
-        $schedule->call(function() {
-            $files = collect(File::allFiles(Storage::disk('backups')->path('Apollo-backup')))
-                ->filter(function($file) {
-                    return $file->getExtension() == 'zip';
-                })
-                ->sortByDesc(function($file) {
-                    return $file->getCTime();
-                })
-                ->map(function($file) {
-                    return $file->getBaseName();
-                });
-            $oldest = $files->reverse()->values()->take(20);
-            Storage::delete($oldest);
-        })->lastDayOfMonth();
-
-        //deletes all csv's Monthly
-        $schedule->call(function() {
-            $files = Storage::files('/public/csv');
-            Storage::delete($files);
-        })->daily();
-
-        //deletes all PDF's Monthly
-        $schedule->call(Report::clean())->weekends();
-
-        $schedule->call(function() {
-            $total = Cache::rememberForever('total_assets', function() {
-                return \App\Models\Asset::count();
-            });
-
-            foreach(Location::all() as $location)
-            {
-                $total = Cache::rememberForever("location_{$location->id}_assets_total", function() {
-                    return \App\Models\Asset::where('location_id', '=', $location->id)->count();
-                });
-            }
-
-        })->daily();
+//        $schedule->call(function() {
+//            $files = collect(File::allFiles(Storage::disk('backups')->path('Apollo-backup')))
+//                ->filter(function($file) {
+//                    return $file->getExtension() == 'zip';
+//                })
+//                ->sortByDesc(function($file) {
+//                    return $file->getCTime();
+//                })
+//                ->map(function($file) {
+//                    return $file->getBaseName();
+//                });
+//            $oldest = $files->reverse()->values()->take(20);
+//            Storage::delete($oldest);
+//        })->lastDayOfMonth();
+//
+//        //deletes all csv's Monthly
+//        $schedule->call(function() {
+//            $files = Storage::files('/public/csv');
+//            Storage::delete($files);
+//        })->daily();
+//
+//        //deletes all PDF's Monthly
+//        $schedule->call(Report::clean())->weekends();
+//
+//        $schedule->call(function() {
+//            $total = Cache::rememberForever('total_assets', function() {
+//                return \App\Models\Asset::count();
+//            });
+//
+//            foreach(Location::all() as $location)
+//            {
+//                $total = Cache::rememberForever("location_{$location->id}_assets_total", function() {
+//                    return \App\Models\Asset::where('location_id', '=', $location->id)->count();
+//                });
+//            }
+//
+//        })->daily();
 
     }
 
