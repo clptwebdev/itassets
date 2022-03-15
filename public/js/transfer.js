0deb137c32c2ac1902fdea00b24291adab2e5006
Elliot <71373207+Elliot-putt@users.noreply.github.com>
2022-03-15 09:08:26 +0000

    transfer js

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -1,9 +1,11 @@
 const transferModal = new bootstrap.Modal(document.getElementById('requestTransfer'));
 
-document.querySelector('.transferBtn').addEventListener('click', function () {
-    const modelID = document.querySelector('#model_id').value = this.getAttribute('data-model-id');
-    const modelTAG = document.querySelector('#asset_tag_transfer').value = this.getAttribute('data-model-tag');
-    const locationID = document.querySelector('#location_id').value = this.getAttribute('data-location-id');
-    const locationFROM = document.querySelector('#location_from').value = this.getAttribute('data-location-from');
+
+document.querySelectorAll('.transferBtn').forEach(elem => elem.addEventListener("click", (event) => {
+
+    const modelID = document.querySelector('#model_id').value = elem.getAttribute('data-model-id');
+    const modelTAG = document.querySelector('#asset_tag_transfer').value = elem.getAttribute('data-model-tag');
+    const locationID = document.querySelector('#location_id').value = elem.getAttribute('data-location-id');
+    const locationFROM = document.querySelector('#location_from').value = elem.getAttribute('data-location-from');
     transferModal.show()
-});
+}));
