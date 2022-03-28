698b19190547de36f3084e7d3e527c793426ae45
Stuart Corns <stuart.corns@tandonta.com>
2022-03-28 11:39:01 +0100

    FFE Index Correction~

--- app/Http/Controllers/FFEController.php
+++ app/Http/Controllers/FFEController.php
@@ -25,16 +25,8 @@ class FFEController extends Controller {
             return to_route('errors.forbidden', ['area', 'FFE', 'view']);
         }
 
-        //Check to see if the user has SUper Admin Role
-        if(auth()->user()->role_id == 1)
-        {
-            //If the User has Super Admin Role/Correct Permissions find all the locations
-            $locations = Location::select('id', 'name')->withCount('ffe')->get();
-        } else
-        {
-            //Else find the locations that the user has been assigned to
-            $locations = auth()->user()->locations->select('id', 'name')->withCount('ffe')->get();
-        }
+        //Find the locations that the user has been assigned to
+        $locations = Location::whereIn('id', auth()->user()->locations->pluck('id'))->select('id', 'name')->withCount('ffe')->get();
 
         //Find the properties that are assigned to the locations the User has permissions to.
         $limit = session('ffe_limit') ?? 25;
