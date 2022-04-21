625baf60be7b8b50a4252532a8473c439dd94921
Stuart Corns <stuart.corns@heathpark.net>
2022-04-21 08:41:58 +0100

    FFES Filter Link Fix

--- app/Http/Controllers/FFEController.php
+++ app/Http/Controllers/FFEController.php
@@ -667,7 +667,7 @@ class FFEController extends Controller {
         $limit = session('ffe_limit') ?? 25;
 
         return view('FFE.view', [
-            "ffes" => $ffe->paginate(intval($limit))->withPath('/accessory/filter')->fragment('table'),
+            "ffes" => $ffe->paginate(intval($limit))->withPath('/ffes/filter')->fragment('table'),
             'statuses' => Status::withCount('ffe')->get(),
             'categories' => Category::withCount('ffe')->get(),
             "locations" => $locations,
